27fba5dbfaa5a4b402c9c3685d22372d3c144ac7
Ismael Neto <ismaelneto49@gmail.com>
2022-08-05 10:15:37 -0300

    Base project

---
+++ src/main/sapo/Facade.java
@@ -0,0 +1,20 @@
+package sapo;
+
+public class Facade {
+
+    /*
+     * TODO
+     * <ul>
+     * <li>Add all controllers</li>
+     * <li>Add each of its methods</li>
+     * </ul>
+     */
+
+    public Facade() {
+        /*
+         * TODO
+         * initialize all controllers
+         */
+    }
+
+}
