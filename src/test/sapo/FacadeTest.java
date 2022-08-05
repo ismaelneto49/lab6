aaebd245a0c4f3104b71e57c6a4bf4f814413047
Ismael Neto <ismaelneto49@gmail.com>
2022-08-05 10:20:02 -0300

    Adding base test file

---
+++ src/test/sapo/FacadeTest.java
@@ -0,0 +1,12 @@
+package sapo;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.fail;
+
+public class FacadeTest {
+
+    @Test
+    void test() {
+        fail();
+    }
+}
