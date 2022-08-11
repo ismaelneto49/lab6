962e51f28cae97193fadab54a740a875f9eeea10
Crisnzx <cristovaon@gmail.com>
2022-08-11 10:06:16 -0300

    chore: Add all TarefaController methods in Facade

--- src/main/sapo/Facade.java
+++ src/main/sapo/Facade.java
@@ -1,20 +1,48 @@
 package sapo;
 
+import sapo.controllers.TarefaController;
+
+import java.util.HashMap;
+
 public class Facade {
 
-    /*
-     * TODO
-     * <ul>
-     * <li>Add all controllers</li>
-     * <li>Add each of its methods</li>
-     * </ul>
-     */
+    private AtividadeController atividadeController;
+    private TarefaController tarefaController;
 
     public Facade() {
-        /*
-         * TODO
-         * initialize all controllers
-         */
+        this.atividadeController = new AtividadeController(new HashMap<>());
+        this.tarefaController = new TarefaController(new HashMap<>(), this.atividadeController);
+    }
+    public String cadastrarTarefa(String idAtividade, String nome, String[] habilidadesRecomendadas) {
+        return this.tarefaController.cadastrarTarefa(idAtividade, nome, habilidadesRecomendadas);
     }
 
-}
+    public void alterarNomeTarefa(String idTarefa, String novoNome) {
+        this.tarefaController.alterarNomeTarefa(idTarefa, novoNome);
+    }
+
+    public void alterarHabilidadesTarefa(String idTarefa, String[] habilidades) {
+        this.tarefaController.alterarHabilidadesTarefa(idTarefa, habilidades);
+    }
+
+    public void adicionarHorasTarefa(String idTarefa, int horas) {
+        this.tarefaController.adicionarHorasTarefa(idTarefa, horas);
+    }
+
+    public void removerHorasTarefa(String idTarefa, int horas) {
+        this.tarefaController.removerHorasTarefa(idTarefa, horas);
+    }
+
+    public String exibirTarefa(String idTarefa) {
+        return this.tarefaController.exibirTarefa(idTarefa);
+    }
+    public void associarPessoaTarefa(String cpf, String idTarefa) {
+        this.tarefaController.associarPessoaTarefa(cpf, idTarefa);
+    }
+
+    public void removerPessoaTarefa(String cpf, String idTarefa) {
+        this.tarefaController.removerPessoaTarefa(cpf, idTarefa);
+    }
+
+
+    }
