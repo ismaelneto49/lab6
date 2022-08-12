36542340b19e2ed682af0844d9b448eb01075b17
Ismael Neto <ismaelneto49@gmail.com>
2022-08-12 10:22:57 -0300

    chore: initial commit

--- src/test/sapo/FacadeTest.java
+++ src/test/sapo/FacadeTest.java
@@ -1,10 +1,19 @@
 package sapo;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import static org.junit.jupiter.api.Assertions.fail;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class FacadeTest {
 
+    private Facade facade;
+
+    @BeforeEach
+    void init() {
+        this.facade = new Facade();
+    }
+
+    //########### Tests for Tarefa (remove later) ###########//
     @Test
     void test() {
         fail();
