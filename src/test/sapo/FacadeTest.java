7ea0e2acd63c9678c97eab7923f512fe90f20425
Ismael Neto <ismaelneto49@gmail.com>
2022-08-12 09:18:28 -0300

    chore: add some tests for Atividade

--- src/test/sapo/FacadeTest.java
+++ src/test/sapo/FacadeTest.java
@@ -1,12 +1,72 @@
 package sapo;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import static org.junit.jupiter.api.Assertions.fail;
+import sapo.entities.Atividade;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 public class FacadeTest {
 
+    private Facade facade;
+
+    @BeforeEach
+    void init() {
+        this.facade = new Facade();
+        facade.cadastrarPessoa("000.000.000-00", "Irmael", new String[]{});
+        facade.cadastrarPessoa("111.111.111-11", "Cristovo", new String[]{});
+    }
+
     @Test
-    void test() {
-        fail();
+    void testCadastrarAtividade() {
+        String id1 = this.facade.cadastrarAtividade("atividade", "descricao", "000.000.000-00");
+        String id2 = this.facade.cadastrarAtividade("legal", "descricao", "111.111.111-11");
+        assertEquals("TVD-0", id1);
+        assertEquals("LGL-1", id2);
+    }
+
+    @Test
+    void testEncerrarAtividade() {
+        String id = this.facade.cadastrarAtividade("atividade", "descricao", "000.000.000-00");
+        this.facade.encerrarAtividade(id);
+    }
+
+    @Test
+    void testEncerrarAtividadeEncerrada() {
+        String id = this.facade.cadastrarAtividade("atividade", "descricao", "000.000.000-00");
+        this.facade.encerrarAtividade(id);
+        assertThrows(IllegalStateException.class, () -> {
+            this.facade.encerrarAtividade(id);
+        });
+    }
+
+    @Test
+    void testEncerrarAtividadeTarefasPendentes() {
+        //TODO
+    }
+
+    @Test
+    void testDesativarAtividade() {
+        String id = this.facade.cadastrarAtividade("atividade", "descricao", "000.000.000-00");
+        this.facade.desativarAtividade(id);
+    }
+
+    @Test
+    void testDesativarAtividadeDesativada() {
+        String id = this.facade.cadastrarAtividade("atividade", "descricao", "000.000.000-00");
+        this.facade.desativarAtividade(id);
+        assertThrows(IllegalStateException.class, () -> {
+            this.facade.desativarAtividade(id);
+        });
+        try {
+            this.facade.desativarAtividade(id);
+        } catch (IllegalStateException ise) {
+            assertEquals("A atividade " +  id + " já está desativada.", ise.getMessage());
+        }
+    }
+
+    @Test
+    void testDesativarAtividadeTarefasPendentes() {
+        //TODO
     }
 }
