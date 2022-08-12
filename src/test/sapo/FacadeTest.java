e828a3dac08f3fc43ac016d3d9f89fb143f7e704
Ismael Neto <ismaelneto49@gmail.com>
2022-08-12 09:55:51 -0300

    chore: add more tests

--- src/test/sapo/FacadeTest.java
+++ src/test/sapo/FacadeTest.java
@@ -4,6 +4,8 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import sapo.entities.Atividade;
 
+import java.util.NoSuchElementException;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 public class FacadeTest {
@@ -16,6 +18,10 @@ public class FacadeTest {
         facade.cadastrarPessoa("000.000.000-00", "Irmael", new String[]{});
         facade.cadastrarPessoa("111.111.111-11", "Cristovo", new String[]{});
     }
+
+    //########### Tests for Pessoa (remove later) ###########//
+
+    //########### Tests for Atividade (remove later) ###########//
 
     @Test
     void testCadastrarAtividade() {
@@ -41,7 +47,7 @@ public class FacadeTest {
         try {
             this.facade.desativarAtividade(id);
         } catch (IllegalStateException ise) {
-            assertEquals("A atividade " +  id + " já está encerrada.", ise.getMessage());
+            assertEquals("A atividade " + id + " já está encerrada.", ise.getMessage());
         }
     }
 
@@ -80,7 +86,7 @@ public class FacadeTest {
         try {
             this.facade.desativarAtividade(id);
         } catch (IllegalStateException ise) {
-            assertEquals("A atividade " +  id + " já está desativada.", ise.getMessage());
+            assertEquals("A atividade " + id + " já está desativada.", ise.getMessage());
         }
     }
 
@@ -100,6 +106,72 @@ public class FacadeTest {
 
     @Test
     void testDesativarAtividadeTarefasPendentes() {
+        String id = this.facade.cadastrarAtividade("atividade", "descricao", "000.000.000-00");
         //TODO
+    }
+
+    @Test
+    void testReabrirAtividade() {
+        String id = this.facade.cadastrarAtividade("atividade", "descricao", "000.000.000-00");
+        this.facade.reabrirAtividade(id);
+    }
+
+    @Test
+    void testReabrirAtividadeAberta() {
+        String id = this.facade.cadastrarAtividade("atividade", "descricao", "000.000.000-00");
+        this.facade.reabrirAtividade(id);
+        assertThrows(IllegalStateException.class, () -> {
+            this.facade.reabrirAtividade(id);
+        });
+        try {
+            this.facade.reabrirAtividade(id);
+        } catch (IllegalStateException ise) {
+            assertEquals("A atividade " + id + " já está aberta", ise.getMessage());
+        }
+    }
+
+    @Test
+    void testExibirAtividade() {
+        String id = this.facade.cadastrarAtividade("atividade", "descricao", "000.000.000-00");
+        String expected = "TVD-0:atividade\n" + "Responsável: Irmael – 000.000.000-00\n" + "===\n" + "descricao\n" + "===\n" + "Tarefas: 0/0\n";
+        assertEquals(expected, this.facade.exibirAtividade(id));
+    }
+
+    @Test
+    void testExibirAtividadeTarefas() {
+        String id = this.facade.cadastrarAtividade("atividade", "descricao", "000.000.000-00");
+        //TODO
+    }
+
+    @Test
+    void testAlterarDescricaoAtividade() {
+        String id = this.facade.cadastrarAtividade("atividade", "descricao", "000.000.000-00");
+        this.facade.alterarDescricaoAtividade(id, "essa atividade é muito legal!");
+    }
+
+    @Test
+    void testAlterarResponsavelAtividade() {
+        String id = this.facade.cadastrarAtividade("atividade", "descricao", "000.000.000-00");
+        this.facade.alterarResponsavelAtividade(id, "000.000.000-00");
+    }
+
+    @Test
+    void testAlterarResponsavelInexistenteAtividade() {
+        String id = this.facade.cadastrarAtividade("atividade", "descricao", "000.000.000-00");
+        assertThrows(NoSuchElementException.class, () -> {
+            this.facade.alterarResponsavelAtividade(id, "cê pê éfe");
+        });
+        try {
+            this.facade.alterarResponsavelAtividade(id, "cê pê éfe");
+        } catch (NoSuchElementException nsee) {
+            assertEquals("CPF fornecido não pertence a nenhuma Pessoa", nsee.getMessage());
+        }
+    }
+
+    //########### Tests for Tarefa (remove later) ###########//
+
+    @Test
+    void test() {
+
     }
 }
